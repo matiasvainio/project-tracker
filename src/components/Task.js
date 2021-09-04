a682d8ca3716b0d58f5a242e2aba5a6c4c2943e8
Matias Vainio <matias.s.vainio@gmail.com>
2021-09-04 15:03:28 +0300

    feature: task options added. single task shows start date, end date and elapsed time

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './task.css';
+import DatePicker from 'react-datepicker';
+import { modify } from '../services/api';
 import obi from '../res/hello-there-obi-wan.gif';
 
 const Task = ({ props }) => {
@@ -20,14 +22,70 @@ const Task = ({ props }) => {
 
 const TaskOptions = ({ task }) => {
   const [editMenu, setEditMenu] = useState(false);
+  const [startDate, setStartDate] = useState(new Date(task.startDate));
+  const [endDate, setEndDate] = useState(new Date(task.endDate));
+  const [elapsedTime, setElapsedTime] = useState(0);
+
   const handleEdit = () => {
     setEditMenu(!editMenu);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTask = {
+      ...task,
+      endDate: endDate,
+      startDate: startDate,
+    };
+    modify(newTask);
+  };
+
+  const calculateElapsedTime = (startDate, endDate) => {
+    return (new Date(endDate) - new Date(startDate)) / 3600000;
+  };
+
+  const formatDate = (date) => {
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    return date.toLocaleString('fi-FI');
+  };
+
+  const changeState = (sel, date) => {
+    sel === 1 ? setStartDate(date) : setEndDate(date);
+    setElapsedTime(calculateElapsedTime(startDate, endDate));
+  };
+
+  useEffect(() => {
+    setElapsedTime(calculateElapsedTime(startDate, endDate));
+  }, [startDate, endDate]);
+
   const editItems = () => {
     return (
       <div id="edit-menu">
-        <img alt="obi" src={obi}></img>
+        <p>Start date</p>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => changeState(1, date)}
+          showTimeSelect
+        />
+        <p>End date</p>
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => changeState(2, date)}
+          showTimeSelect
+        />
+        <button onClick={handleSubmit}>Submit</button>
+        <br />
+        <p>Start date:</p>
+        {formatDate(new Date(startDate))}
+        <p>End date:</p>
+        {formatDate(new Date(endDate))}
+        <p>Elapsed time</p>
+        {elapsedTime}
       </div>
     );
   };
